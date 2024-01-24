125435acb30fe8ea24c9bcc44d98a2f7354a5bcd
arxflay <arxflay@yandex.ru>
2024-01-24 23:41:06 +0100

    Add ARX_NAMESPACE macro

--- include/ArxDefines.h
+++ include/ArxDefines.h
@@ -1,9 +1,11 @@
 #pragma once
 #include <cstdint>
 
-#define ARX_NAMESPACE_BEGIN namespace arx {
+#define ARX_NAMESPACE arx
+
+#define ARX_NAMESPACE_BEGIN namespace ARX_NAMESPACE {
 #define ARX_NAMESPACE_END }
-#define ARX_NAMESPACE_USE using namespace arx
+#define ARX_NAMESPACE_USE using namespace ARX_NAMESPACE
 
 #ifdef WIN32
 #define ARX_IMPORTS __declspec(dllimport)
