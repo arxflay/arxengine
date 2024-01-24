315a9c29c3c2bffab403021fc46a8ccb49635eca
arxflay <arxflay@yandex.ru>
2024-01-24 21:27:55 +0100

    Add ArxException class for ArxEngine exceptions

---
+++ include/ArxException.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "ArxDefines.h"
+#include <string_view>
+#include <string>
+#include <memory>
+
+ARX_NAMESPACE_BEGIN
+
+class ARX_EXPORTS ArxException
+{
+public:
+    enum class ErrorCode : int
+    {
+        NoError = 0,
+        GenericError = 1,
+        EnumToStringError = 2,
+        EnumFromStringError = 3
+    };
+
+    ArxException();
+    ArxException(ErrorCode errCode, std::string_view msg = "Unspecified");
+    virtual ~ArxException() = default;
+
+    virtual std::string ToString() const;
+    virtual std::unique_ptr<ArxException> Clone() const;
+    
+    std::string_view GetMsg() const noexcept;
+    ErrorCode GetErrorCode() const noexcept;
+    int GetErrorCodeInt() const noexcept;
+
+protected:
+    ErrorCode m_errCode;
+    std::string m_msg;
+};
+
+ARX_NAMESPACE_END
---
+++ src/ArxException.cpp
@@ -0,0 +1,25 @@
+#include "ArxException.h"
+ARX_NAMESPACE_BEGIN
+
+ArxException::ArxException()
+    : ArxException(ErrorCode::GenericError)
+{
+}
+
+ArxException::ArxException(ErrorCode errCode, std::string_view msg)
+    : m_errCode(errCode), m_msg(msg)
+{
+}
+
+std::string ArxException::ToString() const
+{
+   return "ErrorCode " + std::to_string(static_cast<int>(m_errCode)) + ", Message: " + m_msg;
+}
+
+std::unique_ptr<ArxException> ArxException::Clone() const { return std::make_unique<ArxException>(*this); }
+
+std::string_view ArxException::GetMsg() const noexcept { return m_msg; }
+ArxException::ErrorCode ArxException::GetErrorCode() const noexcept { return m_errCode; }
+int ArxException::GetErrorCodeInt() const noexcept { return static_cast<int>(m_errCode); }
+
+ARX_NAMESPACE_END
